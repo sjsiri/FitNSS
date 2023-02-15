e887797a85d138a40903d0cafeb9bf0331a9575f
Surinh Siri <surinhjs@gmail.com>
2023-02-15 10:34:39 -0600

    added 4 boxes for create workout

--- web/src/pages/createWorkoutPlan.js
+++ web/src/pages/createWorkoutPlan.js
@@ -11,13 +11,16 @@ class CreateWorkoutPlan extends BindingClass {
 
     constructor() {
         super();
-        this.bindClassMethods(['clientLoaded', 'mount', 'loadExerciseDropDown', 'submit', 'redirectToViewWorkoutPlan', ], this);
+        this.bindClassMethods(['clientLoaded', 'mount', 'loadExerciseDropDown', 'loadExerciseDropDown2', 'loadExerciseDropDown3', 'loadExerciseDropDown4', 'submit', 'redirectToViewWorkoutPlan', ], this);
         this.dataStore = new DataStore();
         this.header = new Header(this.dataStore);
     }
 
     async clientLoaded() {
         this.loadExerciseDropDown();
+        this.loadExerciseDropDown2();
+        this.loadExerciseDropDown3();
+        this.loadExerciseDropDown4();
      }
 
 
@@ -51,6 +54,52 @@ class CreateWorkoutPlan extends BindingClass {
         }
      }
 
+
+     async loadExerciseDropDown2() {
+             // Get All Exercises API
+             const exercises = await this.client.getAllExercises();
+             const exerciseDropDown = document.getElementById('exercisesList2');
+
+             for (let key of exercises) {
+                     let option = document.createElement("option");
+                     option.setAttribute('value', key.exerciseId);
+                     option.setAttribute('innerHTML', key.exerciseName);
+                     let optionText = document.createTextNode(key.exerciseName);
+                     option.appendChild(optionText);
+                     exerciseDropDown.appendChild(option);
+             }
+          }
+
+     async loadExerciseDropDown3() {
+             // Get All Exercises API
+             const exercises = await this.client.getAllExercises();
+             const exerciseDropDown = document.getElementById('exercisesList3');
+
+             for (let key of exercises) {
+                     let option = document.createElement("option");
+                     option.setAttribute('value', key.exerciseId);
+                     option.setAttribute('innerHTML', key.exerciseName);
+                     let optionText = document.createTextNode(key.exerciseName);
+                     option.appendChild(optionText);
+                     exerciseDropDown.appendChild(option);
+             }
+          }
+
+     async loadExerciseDropDown4() {
+             // Get All Exercises API
+             const exercises = await this.client.getAllExercises();
+             const exerciseDropDown = document.getElementById('exercisesList4');
+
+             for (let key of exercises) {
+                     let option = document.createElement("option");
+                     option.setAttribute('value', key.exerciseId);
+                     option.setAttribute('innerHTML', key.exerciseName);
+                     let optionText = document.createTextNode(key.exerciseName);
+                     option.appendChild(optionText);
+                     exerciseDropDown.appendChild(option);
+             }
+          }
+
      /**
         * Method to run when the create Workout plan submit button is pressed. Call the FitNSSClient to
         * create the Exercise.
@@ -60,15 +109,40 @@ class CreateWorkoutPlan extends BindingClass {
         const exerciseList = document.getElementById('exercisesList');
         const exerciseId = document.getElementById('exercisesList').value;
         const exerciseName = exerciseList.options[exerciseList.selectedIndex].innerHTML;
+
+        const exerciseList2 = document.getElementById('exercisesList2');
+        const exerciseId2 = document.getElementById('exercisesList2').value;
+        const exerciseName2 = exerciseList2.options[exerciseList2.selectedIndex].innerHTML;
+
+        const exerciseList3 = document.getElementById('exercisesList3');
+        const exerciseId3 = document.getElementById('exercisesList3').value;
+        const exerciseName3 = exerciseList3.options[exerciseList3.selectedIndex].innerHTML;
+
+        const exerciseList4 = document.getElementById('exercisesList4');
+        const exerciseId4 = document.getElementById('exercisesList4').value;
+        const exerciseName4 = exerciseList4.options[exerciseList4.selectedIndex].innerHTML;
+
         const numberOfSets = document.getElementById('numberOfSets').value;
+        const numberOfSets2 = document.getElementById('numberOfSets2').value;
+        const numberOfSets3 = document.getElementById('numberOfSets3').value;
+        const numberOfSets4 = document.getElementById('numberOfSets4').value;
+
         const numberOfReps = document.getElementById('numberOfReps').value;
+        const numberOfReps2 = document.getElementById('numberOfReps2').value;
+        const numberOfReps3 = document.getElementById('numberOfReps3').value;
+        const numberOfReps4 = document.getElementById('numberOfReps4').value;
+
         const numberOfWeights = document.getElementById('numberOfWeights').value;
+        const numberOfWeights2 = document.getElementById('numberOfWeights2').value;
+        const numberOfWeights3 = document.getElementById('numberOfWeights3').value;
+        const numberOfWeights4 = document.getElementById('numberOfWeights4').value;
+
         const notesBox = document.getElementById('notesBox').value;
 
-        var eList = [exerciseName];
-        var setList = [numberOfSets];
-        var repList = [numberOfReps];
-        var weightList = [numberOfWeights];
+        var eList = [exerciseName, exerciseName2, exerciseName3, exerciseName4];
+        var setList = [numberOfSets, numberOfSets2, numberOfSets3, numberOfSets4];
+        var repList = [numberOfReps, numberOfReps2, numberOfReps3, numberOfReps4];
+        var weightList = [numberOfWeights, numberOfWeights2, numberOfWeights3, numberOfWeights4];
 
         let payload = {workoutDayName: workoutDayName}
 
